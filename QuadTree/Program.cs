7e10da25eef17d579000b6da339d126cc77716a4
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-13 14:24:30 +0200

    read line by line

--- QuadTree/Program.cs
+++ QuadTree/Program.cs
@@ -10,13 +10,21 @@ namespace QuadTree
         {
             List<Point> numbers = new List<Point>();
 
-            try
-            {   // Open the text file using a stream reader.
-                using (StreamReader sr = new StreamReader("input.txt"))
+
+            try { 
+                // Read file using StreamReader. Reads file line by line    
+                using (StreamReader file = new StreamReader("input.txt"))
                 {
-                    // Read the stream to a string, and write the string to the console.
-                    String line = sr.ReadToEnd();
-                    Console.WriteLine(line);
+                    int counter = 0;
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(ln);
+                        counter++;
+                    }
+                    file.Close();
+                    Console.WriteLine(counter + "File has {counter} lines.");
                 }
             }
             catch (IOException e)
