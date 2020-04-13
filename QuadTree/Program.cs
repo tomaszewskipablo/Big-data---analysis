8333d1eb75e4ca9a7ff65f0658a31b1c3d952320
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-13 14:07:55 +0200

    read lines from file

--- QuadTree/Program.cs
+++ QuadTree/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace QuadTree
 {
@@ -6,7 +8,23 @@ namespace QuadTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Point> numbers = new List<Point>();
+
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("input.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
