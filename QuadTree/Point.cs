c7006873a9ad775802c3c348e208bf47ab2c76d9
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-13 13:57:37 +0200

    implement points class

---
+++ QuadTree/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadTree
+{
+    public class Point
+    {
+        double x;
+        double y;
+        double z;
+        short i;
+        public Point()
+        {
+        }
+        public Point(double x, double y, double z, short i)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.i = i;
+        }
+    }
+}
+
