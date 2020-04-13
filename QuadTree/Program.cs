d0d04254a4015c42aa73b50f7b68747a1c5cdfdf
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-13 13:49:37 +0200

    create project console app (.Net core)

---
+++ QuadTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuadTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
